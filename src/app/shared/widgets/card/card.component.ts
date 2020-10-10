b9447afe2e70836b4e2ee0098eb87818e6e1800a
Azafo Cossa <azafoalexandre@outlook.com>
2020-10-10 13:01:55 +0200

    area chart card widget

---
+++ src/app/shared/widgets/card/card.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-widget-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit {
+  @Input() label: string;
+  @Input() total: string;
+  @Input() percentage: string;
+  @Input() data = [];
+
+  chartOptions = {};
+  Highcharts = Highcharts;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        type: 'area',
+        backgroundColor: null,
+        borderWidth: 0,
+        margin: [2, 2, 2, 2],
+        height: 80,
+      },
+      title: {
+        text: null,
+      },
+      subtitle: {
+        text: null,
+      },
+      legend: { enabled: false },
+      tooltip: {
+        split: true,
+        outside: true,
+      },
+      xAxis: {
+        labels: { enabled: false },
+        title: { text: null },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: [],
+      },
+      yAxis: {
+        labels: { enabled: false },
+        title: { text: null },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: [],
+      },
+      credits: { enabled: false },
+      exporting: { enabled: false },
+      series: [
+        {
+          data: this.data,
+        },
+      ],
+    };
+    HC_exporting(Highcharts);
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 300);
+  }
+}

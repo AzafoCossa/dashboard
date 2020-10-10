3e902cc2d0f6b4701b055b5c6703401ac2a29ea5
Azafo Cossa <azafoalexandre@outlook.com>
2020-10-10 07:45:38 +0200

    Custom module for the dashboard

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,4 +1,9 @@
-import { AppRoutingModule } from './../../app-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SharedModule } from './../../shared/shared.module';
+import { PostsComponent } from './../../modules/posts/posts.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,7 +12,15 @@ import { DefaultComponent } from './default.component';
 import { DashboardComponent } from './../../modules/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [DefaultComponent, DashboardComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [DefaultComponent, DashboardComponent, PostsComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+  ],
 })
 export class DefaultModule {}
